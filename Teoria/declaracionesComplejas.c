b81628ece6d9e7426531500fef2e77139527b67f
DarkRyan721 <86139190+DarkRyan721@users.noreply.github.com>
2023-08-25 07:43:14 -0500

    Create declaracionesComplejas.c

---
+++ Teoria/declaracionesComplejas.c
@@ -0,0 +1,12 @@
+// printf prototype: printf(const char* restric format, ...); 
+// scanf prototype: scanf(const char* format,...)
+
+// int (* func_ptr) (const char* restric format, ...); Apuntador a una funcion con parametros que retorna un entero
+// int * func_ptr (const char* restric format, ...); Funcion con parametros que retorna un apuntador
+
+
+
+
+
+
+
