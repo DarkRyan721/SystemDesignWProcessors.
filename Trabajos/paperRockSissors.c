89b28203c47e61fd140425948a103896a395e315
DarkRyan721 <86139190+DarkRyan721@users.noreply.github.com>
2023-08-25 08:30:51 -0500

    Update paperRockSissors.c

--- Trabajos/paperRockSissors.c
+++ Trabajos/paperRockSissors.c
@@ -3,3 +3,11 @@ char *AB[3][3] = {{"e", "B", "A"}, {"A", "e", "B"}, {"B", "A", "e"}};
 int rockPaperScissor(int A, int B) { return printf("%s\n", AB[A][B]); }
 int (*pointerTo)(int, int) = rockPaperScissor;
 int main() { return pointerTo(1, 2); }
+
+/*
+#include <stdio.h>
+void empate() {printf("empate");} void ganaA()  {printf("gana A");} void ganaB()  {printf("gana B");}
+void (*AB[3][3])() = {{empate, ganaB, ganaA}, {ganaA, empate, ganaB}, {ganaB, ganaA, empate}};
+int main(){*AB[1][1];}
+*/
+
